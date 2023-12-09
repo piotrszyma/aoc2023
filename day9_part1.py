efdcbddba5dba838819a8a24df90dcf0930a5078
Piotr Szyma <pszyma@opera.com>
2023-12-09 22:43:55 +0100

    feat: day9 part 1

--- day9_part1.py
+++ day9_part1.py
@@ -19,16 +19,39 @@ def _generate_sequences(dataset: list[int]) -> list[list[int]]:
 
 
 def main():
-    data = pathlib.Path("day9_input_test.txt").read_text()
+    data = pathlib.Path("day9_input.txt").read_text()
     datasets_raw = data.split('\n')
     datasets: list[list[int]] = []
     for dataset_raw in datasets_raw:
         dataset = [int(n) for n in dataset_raw.split(' ')]
         datasets.append(dataset)
 
+    next_values_sum = 0
+
     for dataset in datasets:
         sequences = _generate_sequences(dataset)
-        filled_by_one =
+
+        extrapolated_values = []
+
+        for idx, sequence in enumerate(sequences[::-1]):
+            if idx == 0:
+                extrapolated_values.append(0)
+                continue
+
+            last_item = sequence[-1]
+            previous_extrapolated = extrapolated_values[-1]
+
+            value = last_item + previous_extrapolated
+
+            extrapolated_values.append(value)
+
+        next_value = extrapolated_values[-1]
+
+        next_values_sum += next_value
+
+    print(next_values_sum)
+
+
 
 if __name__ == "__main__":
     main()
